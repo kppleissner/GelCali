25338b9a4fd671114ad5b442998005439c75cfc7
Klaus-Peter Pleissner <44700736+kppleissner@users.noreply.github.com>
2021-04-19 21:32:49 +0200

    Update app.R
    new version from Rstudio.cloud

--- app.R
+++ app.R
@@ -18,32 +18,30 @@ library(knitr)
 library(pander)
 library(readxl)
 
-# Define UI for application that draws a histogram
+##############################################################################################
 ui <- fluidPage(
-    
     # Application title
-    h1(id="big-heading", "Calibration of 2-dimensional electrophoresis gels"),
+    h1(id="big-heading", "Calibration of 2-dimensional electrophoresis gels",img(src = "2DE_Template.png", height =30, width = 50) ),
     tags$style(HTML("#big-heading{color: blue;}")),
     h3(id="small-heading", "Linear regression of pI-axis and logarithmic regression of Mr-axis"),
     tags$style(HTML("#small-heading{color: black;}")),
     
-    
-    
     sidebarLayout(
         sidebarPanel(
-            
-            HTML('<p>For the calibration of pI | Mr-axis you can use manually given pI | Mr-values and distances (see below) or data stored in an external EXCEL-file. 
-            A template of such EXCEL-file (.xlsx)  can be found on  <a href="https://github.com/kppleissner/GelCali" target="_blank"> GitHub</a>.'), 
+            HTML('<p>For the calibration of pI | Mr-axes you can use manually given pI | Mr-values and distances (see below) or data stored in an external EXCEL-file. 
+            Templates of <a href="https://github.com/kppleissner/GelCali/blob/main/GelCali_test_table.xlsx" target="_blank"> EXCEL-file (.xlsx)</a> and   <a href="https://github.com/kppleissner/GelCali/blob/main/2DE_Template1.PNG" target="_blank"> 2-DE Gel Image  (.png)</a> can be found on Github.
+            <br> You can download the regression plot as a (.png) using the camera sign of plotly-functionalities visible by moving the mouse up above the regression curve. <br> The R-code is given on  <a href="https://github.com/kppleissner/GelCali/" target="_blank"> GitHub</a>.
+                 '), 
             br(),
             br(),
-            tags$b("Please enter distance and pI-values for selected spots:"),
+            tags$b("Please enter distance and pI-values for selected protein spots:"),
             br(),
             textInput("x", "distance [mm or pixels]", value = "15, 30, 77, 118, 150,200", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
-            textInput("y", "pI-value", value = "3.5, 4.2, 5.6, 7.3, 8.3,11", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
+            textInput("y", "pI-values", value = "3.5, 4.2, 5.6, 7.3, 8.3,11", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
             hr(),
-            tags$b("Please enter distance and Mr-values for selected spots:"),
+            tags$b("Please enter distance and Mr-values for selected protein spots:"),
             textInput("xx", "distance [mm or pixels]", value = "2, 167, 95, 65, 55, 250", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
-            textInput("yy", "Mr-value", value = "80000, 15200, 25000, 33000, 36000, 14200", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
+            textInput("yy", "Mr-values", value = "80000, 15200, 25000, 33000, 36000, 14200", placeholder = "Enter values separated by a comma with decimals as points, e.g. 4.2, 4.4, 5, 5.03, etc."),
             hr(),
             checkboxInput("check", "Check if you want to use  EXCEL file for calibration", FALSE),
             fileInput('uploadfile',"Upload Calibration data as EXCEL-file: .xlsx",  accept = c(".xlsx")), 
@@ -53,12 +51,9 @@ ui <- fluidPage(
             HTML('<p>The calibration of 2-dimensional electrophoresis gels was developed by <a href="https://kppleissner.github.io"> Dr. Klaus-Peter Pleissner</a>
           and is based on code template of <a href="https://www.antoinesoetewey.com/">Antoine Soetewey</a>.
           The original code is available  on <a href="https://github.com/AntoineSoetewey/statistics-202" target="_blank">GitHub</a>.')
-            
-            
         ),
         
         mainPanel(
-            # tags$b("Your data for pI-values:"),
             tags$b("Your data used for calibration of pI-axis:"),
             br(),
             DT::dataTableOutput("tbl"),
@@ -71,7 +66,6 @@ ui <- fluidPage(
             tags$b("Computed parameters:"),
             br(),
             br(),
-            #tags$b("Computed parameters:"),
             uiOutput("results"),
             br(),
             tags$b("Linear regression plot for pI-values"),
@@ -90,7 +84,6 @@ ui <- fluidPage(
             uiOutput("loga_formula"),
             br(),
             tags$b("Computed parameters:"),
-            #verbatimTextOutput("summary"),
             br(),
             br(),
             uiOutput("loga_result"),
@@ -104,8 +97,7 @@ ui <- fluidPage(
     )
 )
 
-
-
+####################################################################################
 
 server <- function(input, output) {
     
@@ -129,7 +121,6 @@ server <- function(input, output) {
     output$tbl <- DT::renderDataTable({
         
         if(input$check){
-            #if( !exists("data_excel()") ){
             y <-  data_excel()$pI
             x <-  data_excel()$distance_pI
         }else{
@@ -154,7 +145,6 @@ server <- function(input, output) {
     
     # Data output for Mr-values
     output$tbllog <- DT::renderDataTable({
-        
         if(input$check){
             y <-  data_excel()$Mr
             x <-  data_excel()$distance_Mr
@@ -223,6 +213,8 @@ server <- function(input, output) {
             )
         }
     })
+    
+    
     # Output of formula of linear regression  
     output$formula <- renderUI({
         if( input$check){
@@ -239,8 +231,6 @@ server <- function(input, output) {
             paste0("\\(  y = \\beta_0 + \\beta_1 * x = \\) ", round(fit$coef[[1]], 3), " + ", round(fit$coef[[2]], 3), "  \\(  *  x \\)")
         )
     })
-    
-    
     
     #output of parameters of linear regression
     output$results <- renderUI({
@@ -364,9 +354,8 @@ server <- function(input, output) {
         ggplotly(p)
     })
     
-    
-    
 } #end server
+#####################################################################
 
 # Run the application
 shinyApp(ui = ui, server = server)
